0820b58a243fc2694c5ebf591eb35b970ae05609
bryce-eldridge <bryce.eldridge121003@gmail.com>
2022-08-09 15:02:18 +1000

    Main Menu and Tutorial, Plus Plates

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,8 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    public void playGame()
+    private void OnTriggerEnter(Collider other)
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+   
 }
